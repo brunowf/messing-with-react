2fd095a7825ba703779e151174beb4d9239ba53a
Bruno Weiler Foletto <brunoweiller@hotmail.com>
2020-02-24 01:42:00 -0300

    Having some fun

--- src/index.js
+++ src/index.js
@@ -1,28 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Dialog } from 'primereact/dialog';
+import { RadioButton } from 'primereact/radiobutton';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 import './index.css';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 function Square(props) {
-  return (
-    <button
-      className="square"
-      onClick={props.onClick}
-    >
-      {props.value}
-    </button>
-  );
+  let colorList;
+  let color = 'square';
+  switch (props.color) {
+    case 'G':
+      color += 'Green';
+      colorList = props.cardList.green;
+      break;
+    case 'B':
+      color += 'Blue';
+      colorList = props.cardList.blue;
+      break;
+    case 'P':
+      color += 'Purple';
+      colorList = props.cardList.purple;
+      break;
+    case 'O':
+      color += 'Orange';
+      colorList = props.cardList.orange;
+      break;
+    default:
+      colorList = props.cardList.green;
+      break;
+  }
+  if (colorList !== undefined) {
+    const mapping = colorList.map((list, step) => {
+      return (
+        <button
+          key={step}
+          className={color}
+        >
+          {list.quantity}
+        </button>
+      );
+    })
+    return (
+      <ol>{mapping}</ol>
+    );
+  } else {
+    return null;
+  }
 }
 
 class Board extends React.Component {
 
   renderSquare(i) {
     return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => {
-          this.props.onClick(i)
-        }}
-      />
+      <div>
+        <Square
+          cardList={this.props.cardList}
+          test={1}
+          color={i}
+        />
+      </div>
     );
   }
 
@@ -30,19 +70,16 @@ class Board extends React.Component {
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare('G')}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare('B')}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare('P')}
+        </div>
+        <div className="board-row">
+          {this.renderSquare('O')}
         </div>
       </div>
     );
@@ -53,100 +90,175 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      stepNumber: 0,
-      xIsNext: true,
+      visible: true,
+      cardList: {
+        // blue: [{
+        //   name: 'b',
+        //   quantity: 2,
+        // }],
+        // purple: [{
+        //   name: 'p',
+        //   quantity: 3,
+        // }],
+        // orange: [{
+        //   name: 'o',
+        //   quantity: 1,
+        // }],
+      },
+      newCardname: '',
+      newCardrarity: 'G',
+      newCardquantity: 0,
     }
-  }
-
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    })
   }
 
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ? 'Go to move #' + move : 'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => {
-            this.jumpTo(move)
-          }}>
-            {desc}
-          </button>
-        </li>
-      );
-    })
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
 
     return (
       <div className="game">
         <div className="game-board">
           <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            cardList={this.state.cardList}
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <div>
+            <button onClick={() =>
+              this.setState({ visible: true })
+            }>alou</button>
+          </div>
+          <Dialog
+            header="Card"
+            visible={this.state.visible}
+            onHide={() => {
+              this.setState({ newCardname: '', newCardrarity: 'G', newCardquantity: 0, visible: false });
+            }}
+            style={{ width: '25vw' }}
+            modal={true}
+          >
+            <div className="status">
+              <RadioButton
+                inputId="rb1"
+                value="G"
+                name="Common"
+                onChange={(e) => this.setState({ newCardrarity: e.value })}
+                checked={this.state.newCardrarity === 'G'}
+              />
+              <label htmlFor="rb1" className="p-radiobutton-label">Common  </label>
+              <RadioButton
+                inputId="rb2"
+                value="B"
+                name="Rare"
+                onChange={(e) => this.setState({ newCardrarity: e.value })}
+                checked={this.state.newCardrarity === 'R'}
+              />
+              <label htmlFor="rb2" className="p-radiobutton-label">Rare  </label>
+              <RadioButton
+                inputId="rb3"
+                value="P"
+                name="Epic"
+                onChange={(e) => this.setState({ newCardrarity: e.value })}
+                checked={this.state.newCardrarity === 'P'}
+              />
+              <label htmlFor="rb3" className="p-radiobutton-label">Epic  </label>
+              <RadioButton
+                inputId="rb4"
+                value="O"
+                name="Legendary"
+                onChange={(e) => this.setState({ newCardrarity: e.value })}
+                checked={this.state.newCardrarity === 'O'}
+              />
+              <label htmlFor="rb4" className="p-radiobutton-label">Legendary</label>
+            </div>
+            <div>
+              <InputText value={this.state.newCardname} onChange={(e) => this.setState({ newCardname: e.target.value })} />
+              <InputText keyfilter="pint" value={this.state.newCardquantity} onChange={(e) => this.setState({ newCardquantity: parseInt(e.target.value) })} />
+              <Button
+                label="Add"
+                icon="pi pi-check"
+                onClick={() => {
+
+                  let list = null;
+                  const cardListAux = this.state.cardList;
+                  switch (this.state.newCardrarity) {
+                    case 'G':
+                      if (this.state.cardList.green === undefined) {
+                        list = [{
+                          name: this.state.newCardname,
+                          quantity: this.state.newCardquantity,
+                        }];
+                        cardListAux.green = list;
+                      } else {
+                        list = this.state.cardList.green.slice();
+                        cardListAux.green = list.concat([{
+                          name: this.state.newCardname,
+                          quantity: this.state.newCardquantity,
+                        }]);
+                      }
+                      break;
+                    case 'B':
+                      if (this.state.cardList.blue === undefined) {
+                        list = [{
+                          name: this.state.newCardname,
+                          quantity: this.state.newCardquantity,
+                        }];
+                        cardListAux.blue = list;
+                      } else {
+                        list = this.state.cardList.blue.slice();
+                        cardListAux.blue = list.concat([{
+                          name: this.state.newCardname,
+                          quantity: this.state.newCardquantity,
+                        }]);
+                      }
+                      break;
+                    case 'P':
+                      if (this.state.cardList.purple === undefined) {
+                        list = [{
+                          name: this.state.newCardname,
+                          quantity: this.state.newCardquantity,
+                        }];
+                        cardListAux.purple = list;
+                      } else {
+                        list = this.purple.cardList.purple.slice();
+                        cardListAux.green = list.concat([{
+                          name: this.state.newCardname,
+                          quantity: this.state.newCardquantity,
+                        }]);
+                      }
+                      break;
+                    case 'O':
+                      if (this.state.cardList.orange === undefined) {
+                        list = [{
+                          name: this.state.newCardname,
+                          quantity: this.state.newCardquantity,
+                        }];
+                        cardListAux.orange = list;
+                      } else {
+                        list = this.state.cardList.orange.slice();
+                        cardListAux.orange = list.concat([{
+                          name: this.state.newCardname,
+                          quantity: this.state.newCardquantity,
+                        }]);
+                      }
+                      break;
+                    default:
+                      break;
+                  }
+                  if (list !== null) {
+
+                    this.setState({
+                      cardList: cardListAux,
+                      visible: false
+                    });
+                  }
+                }}
+              />
+            </div>
+          </Dialog>
+          <ol>{}</ol>
         </div>
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a]
-      && squares[a] === squares[b]
-      && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 // ========================================
